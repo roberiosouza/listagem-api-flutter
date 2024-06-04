86d91b15fe7fa969c990c9700223f440f4f3367d
Robério <rroberiosouza@gmail.com>
2024-06-04 09:10:56 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lista_placeholder
+
+Projeto para consumir API placeholder e apresentar numa listagem
---
+++ android/app/src/main/kotlin/com/example/lista_placeholder/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lista_placeholder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
